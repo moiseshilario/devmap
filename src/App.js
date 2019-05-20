e104910344ecd3a8caf857b0934852d0b0600206
moiseshilario <moiseshilariu@gmail.com>
2019-05-20 01:31:14 -0300

    Add reactotron config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
+
+import store from './store';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
+import GlobalStyle from './styles/global';
 
 import Main from './pages/Main';
 
-const App = () => <Main />;
+const App = () => (
+  <Provider store={store}>
+    <GlobalStyle />
+    <Main />
+  </Provider>
+);
 
 export default App;
